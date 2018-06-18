a4075977bbc4fd43a55a2b5f45e94f432f49b0e1
Lin Yang <lin.yang@rea-group.com>
2018-06-19 02:57:43 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,21 +3,22 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-// var cardThree = cards[2];
-// var cardFour = cards[3];
-
-cardsInPlay.push(cards[0]);
-console.log("User flipped " + cards[0]);
-cardsInPlay.push(cards[2]);
-console.log("User flipped " + cards[2]);
-
-if (cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
-  } 
-  else{
-    alert("Sorry, try again");
+var checkForMatch = function () {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again");
+    }
   }
 }
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
