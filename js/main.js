00617a5a5b6e1ef24276de0e4b875c3045b2aac3
Lin Yang <lin.yang@rea-group.com>
2018-06-20 00:08:51 +1000

    add reset button and user's score

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("Up and running!");
-
 var cards = [
   {
     rank: "queen",
@@ -24,15 +22,18 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var userScore = 0;
 
 var checkForMatch = function (elem) {
-  elem.setAttribute('src', cardsInPlay[cardsInPlay.length - 1].cardImage);
+  var comPareOne = cardsInPlay[cardsInPlay.length - 1];
+  var comPareTwo = cardsInPlay[cardsInPlay.length - 2];
+  elem.setAttribute('src', comPareOne.cardImage);
   if (cardsInPlay.length >= 2) {
-    var comPareOne = cardsInPlay[cardsInPlay.length - 1];
-    var comPareTwo = cardsInPlay[cardsInPlay.length - 2];
     if (comPareOne.rank === comPareTwo.rank) {
       if (comPareOne.suit !== comPareTwo.suit) {
         alert("You found a match!");
+        userScore++;
+        document.getElementById("user-score").innerHTML = userScore;
       }
     }
     else {
@@ -55,10 +56,20 @@ var createBoard = function () {
     var cardElement = document.createElement('img');
     cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', function () { flipCard(this);});
+    cardElement.addEventListener('click', function () { flipCard(this); });
     document.getElementById("game-board").appendChild(cardElement);
+    document.getElementById("user-score").innerHTML = userScore;
   }
-
-
 }
+
+var resetGame = function () {
+  var nodeCards = document.getElementById("game-board").childNodes;
+  for (i = 0; i < nodeCards.length; i++) {
+    nodeCards[i].setAttribute('src', 'images/back.png');
+  }
+  cardsInPlay = [];
+  userScore = 0;
+  document.getElementById("user-score").innerHTML = userScore;
+}
+
 createBoard();
